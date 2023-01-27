f43909bd0949d6a8cf4edc84f0f074ccdda56e70
Mateus Pegoraro <mateus.pegoraro@involves.com>
2023-01-27 07:59:10 -0300

    node --start

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from 'node:http' // utilizando o prefixo "node:" como boa prática
 
 const server = http.createServer((req, res) => {
-    return res.end("hello word")
+    return res.end("hello ignite")
 })
 
 server.listen(3333)
