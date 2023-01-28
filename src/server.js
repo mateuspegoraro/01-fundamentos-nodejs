cf862ab9f15596006d226c988d9123100a7a3157
Mateus Pegoraro <mateus.pegoraro@involves.com>
2023-01-28 11:02:52 -0300

    Rotas HTTP

--- src/server.js
+++ src/server.js
@@ -1,6 +1,27 @@
 import http from 'node:http' // utilizando o prefixo "node:" como boa prática
 
+//  Principais recursos de uma chamada HTTP
+//   - HTTP
+//     - Método HTTP
+//     - URL
+
+// GET => Buscar um recurso
+// POST => Criar um recurso
+// PUT => Editar um recurso
+// PATCH => Atualizar uma informação específica de um recurso
+// DELETE => Deletar um recurso
+
 const server = http.createServer((req, res) => {
+    const {method, url} = req
+
+    if(method === 'GET' && url === '/users'){
+        return res.end('Listagem de usuários')
+    }
+
+    if(method === 'POST' && url === '/users'){
+        return res.end('Criação de usuário')
+    }
+
     return res.end("hello ignite")
 })
 
