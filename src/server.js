da0352267d46fd40b8c03741325928e4c31b5322
Mateus Pegoraro <mateus.pegoraro@involves.com>
2023-01-28 11:39:57 -0300

    Métodos HTTP e Headers

--- src/server.js
+++ src/server.js
@@ -11,14 +11,28 @@ import http from 'node:http' // utilizando o prefixo "node:" como boa prática
 // PATCH => Atualizar uma informação específica de um recurso
 // DELETE => Deletar um recurso
 
+// Cabeçalhos da Request e da Response são Metadados (informações adicionais de como o dado pode ser interpretado pelo cliente)
+
+const users = []
+
 const server = http.createServer((req, res) => {
     const {method, url} = req
 
+
+    console.log(req.headers)
+    
     if(method === 'GET' && url === '/users'){
-        return res.end('Listagem de usuários')
+        return res
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
     }
 
     if(method === 'POST' && url === '/users'){
+        users.push({
+            id: 1,
+            name: 'john doe',
+            email: 'johndoe@example.com'
+        })
         return res.end('Criação de usuário')
     }
 
