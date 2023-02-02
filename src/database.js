7dab42755fe83a49255f5307fa0c9d6b63f083d3
Mateus Pegoraro <mateus.pegoraro@involves.com>
2023-02-02 07:51:25 -0300

    Persistindo banco de dados

--- src/database.js
+++ src/database.js
@@ -1,5 +1,22 @@
+import fs from 'node:fs/promises'
+
+//console.log(import.meta.url)
+
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
     #database = {} // o # impede que o atributo seja acessado externamente
+
+    constructor() {
+        fs.readFile(databasePath, 'utf-8').then(data => {
+            this.#database = JSON.parse(data)
+        }).catch(() => {
+            this.#persist()
+        })
+    }
+    #persist(){
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     select(table) {
         const data = this.#database[table] ?? []
@@ -13,6 +30,7 @@ export class Database {
         } else {
             this.#database[table] = [data]
         }
+        this.#persist()
         return data
     }
 
