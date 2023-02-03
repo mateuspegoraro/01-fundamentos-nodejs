2f9c311891174202941bbee8ef12e091462c48fb
Mateus Pegoraro <mateus.pegoraro@involves.com>
2023-02-03 07:18:15 -0300

    Separando rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -11,36 +11,25 @@
 
 // Cabeçalhos da Request e da Response são Metadados (informações adicionais de como o dado pode ser interpretado pelo cliente)
 
-import { randomUUID } from 'node:crypto'
-import http from 'node:http' // utilizando o prefixo "node:" como boa prática
-import { Database } from './database.js'
-import { json } from './middlewares/json.js'
 
-const database = new Database()
+import http from 'node:http' // utilizando o prefixo "node:" como boa prática
+import { json } from './middlewares/json.js'
+import { routes } from './routes.js'
+
+
 
 const server = http.createServer(async (req, res) => {
     const {method, url} = req
 
     await json(req, res)
 
-    if(method === 'GET' && url === '/users'){
-        const users = database.select('users')
-        return res
-            .end(JSON.stringify(users))
-    }
+    const route = routes.find(route => {
+        return route.method === method && route.path === url
+    })
 
-    if(method === 'POST' && url === '/users'){
-        const {name, email} = req.body
-        const user = {
-            id: randomUUID(),
-            name: name,
-            email: email
-        }
-        database.insert('users', user)
-
-        return res
-            .writeHead(201)
-            .end()
+    console.log(route)
+    if(route) {
+        return route.handler(req, res)
     }
 
     return res.writeHead(404).end()
